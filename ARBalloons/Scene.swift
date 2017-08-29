668e73e402b027bf43b19aa2bf9c379f8f670f57
Marla Narazani <marla.narazani@ibm.com>
2017-08-29 02:02:17 +0200

    added some floating

--- ARBalloons/Scene.swift
+++ ARBalloons/Scene.swift
@@ -32,33 +32,39 @@ class Scene: SKScene {
         
         // Get the first node
         if let node = clicked.first {
-            //Set falldown action and fade as a group
-            let move =  SKAction.move(to: CGPoint(x: node.position.x - 5, y: node.position.y - 200), duration: 0.7)
-            let fadeOut = SKAction.fadeOut(withDuration: 0.7)
-            let groupAction = SKAction.group([move,fadeOut])
+            //Set falldown actions
+            let moveDown =  SKAction.move(to: CGPoint(x: node.position.x - 5, y: node.position.y - 100), duration: 0.4)
+            let moveLeftDown =  SKAction.move(to: CGPoint(x: node.position.x - 20, y: node.position.y - 140), duration: 0.3)
+            let moveRightDown =  SKAction.move(to: CGPoint(x: node.position.x + 20, y: node.position.y - 140), duration: 0.3)
+            let moveToBottom =  SKAction.move(to: CGPoint(x: node.position.x + 45, y: node.position.y - 220), duration: 0.3)
+            //make some balloons float to the left and some to the right when falling
+            let moveDownFloating = ((arc4random() % 2)==0) ? moveLeftDown : moveRightDown
+            
+            let sequence = SKAction.sequence([moveDown, moveDownFloating, moveToBottom])
+            let fadeOut = SKAction.fadeOut(withDuration: 1.0)
+            let groupAction = SKAction.group([sequence,fadeOut])
             
             node.run(groupAction){
                 //callback to delete node
                 node.removeFromParent()
             }
         } else {
-        //create new balloon if elsewhere selected
-        guard let sceneView = self.view as? ARSKView else {
-            return
-        }
-        
-        // Create anchor using the camera's current position
-        if let currentFrame = sceneView.session.currentFrame {
+            guard let sceneView = self.view as? ARSKView else {
+                return
+            }
             
-            // Create a transform with a translation of 0.2 meters in front of the camera
-            var translation = matrix_identity_float4x4
-            translation.columns.3.z = -0.4
-            let transform = simd_mul(currentFrame.camera.transform, translation)
-            
-            // Add a new anchor to the session
-            let anchor = ARAnchor(transform: transform)
-            sceneView.session.add(anchor: anchor)
-        }
+            // Create anchor using the camera's current position
+            if let currentFrame = sceneView.session.currentFrame {
+                
+                // Create a transform with a translation of 0.4 meters in front of the camera
+                var translation = matrix_identity_float4x4
+                translation.columns.3.z = -0.4
+                let transform = simd_mul(currentFrame.camera.transform, translation)
+                
+                // Add a new anchor to the session
+                let anchor = ARAnchor(transform: transform)
+                sceneView.session.add(anchor: anchor)
+            }
         }
     }
 }
